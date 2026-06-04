0987800f07af5bc9396e3ea3fed261ea6119d33f
Onno Holzmann <143614975+onnoholzmann@users.noreply.github.com>
2026-06-05 00:25:58 +0200

    made it solve the equations of motion

--- calc_plot_points.py
+++ calc_plot_points.py
@@ -1,5 +1,3 @@
-from scipy.integrate import quad
-import scipy.constants as constants
 import scipy
 # imported the things that are used more often seperately, but might be better to use either full aliasing, or none
 import numpy
@@ -11,18 +9,18 @@ you can go for natural coords with
 c=G=M=1
 or you could use the classical values
 """
-a_4 = 94/3 - 41/32*constants.pi**2
+a_4 = 94/3 - 41/32*scipy.constants.pi**2
 gamma = (a_4 + 4) / 2
 c = 1
 G = 1
 M = 1
 """
-c = constants.c
-G = constants.G
-M = constants.M
+c = scipy.constants.constants.c
+G = scipy.constants.constants.G
+M = scipy.constants.constants.M
 """
 
-
+# prob needs to be removed
 @cache
 def get_u(R):
   return (G*M) / (c**2*R)
@@ -37,10 +35,10 @@ def calc_A_derivative(nu, u):
   return -2*(1-gamma*u) / (1-gamma*u-2*nu*u**3) + (1-2*u) * (-4*nu*gamma*u**3+6*nu*u**2) / ((1-gamma*u-2*nu*u**3)**2)
 
 def integrand(R, nu):
-  return numpy.sqrt(calc_D(R, nu))/calc_A(R, nu)
+  return numpy.sqrt(calc_D(nu, 1/R))/calc_A(nu, 1/R)
 
 def calc_tortoise(R, nu, R0):
-  tortoise = quad(integrand, R0, R, args=(nu))
+  tortoise = scipy.integrate.quad(integrand, R0, R, args=(nu))
   return tortoise
 
 def calc_squared_angular_momentum(nu, u):
@@ -70,7 +68,49 @@ def calc_z_3(nu):
 
 # H is the hamiltonian
 def calc_H_eff(p_r, p_varphi, nu, r):
-  return numpy.sqrt(p_r**2 + calc_A(nu, r) * (1 + p_varphi**2/r**2 + calc_z_3(nu) * p_r**4/r**2))
+  return numpy.sqrt(p_r**2 + calc_A(nu, 1/r) * (1 + p_varphi**2/r**2 + calc_z_3(nu) * p_r**4/r**2))
 
 def calc_H(p_r, p_varphi, nu, r):
   return 1/nu * numpy.sqrt(1 + 2*nu*(calc_H_eff(p_r, p_varphi, nu, r) - 1))
+
+# nu = mu / M
+# nu is the mass ratio
+nu = 1/4
+# since nu is constant, z_3 is also constant
+z_3 = calc_z_3(nu)
+
+def derivatives(t, y):
+  r, varphi, p_varphi, p_r = y
+
+  # calc all the values used in the derivatives
+  u=1/r
+  A = calc_A(nu, u)
+  A_derivative = calc_A_derivative(nu, u)
+  D = calc_D(nu, u)
+  H = calc_H(p_r, p_varphi, nu, r)
+  H_eff = calc_H_eff(p_r, p_varphi, nu, r)
+
+  # the equations of motion
+  dvarphi_dt = (A*p_varphi) / (nu*r**2*H*H_eff)
+  dr_dt = A/numpy.sqrt(D) * 1/(nu*H*H_eff) * (p_r + z_3 * (2*A)/(r**2) * p_r**3)
+  dp_varphi_dt = 0
+  # it is assumed 0, since we assume no radiation(friction in this case), at least for now
+  dp_r_dt = -A/numpy.sqrt(D) * 1/(2*nu*H*H_eff) * (A_derivative + p_varphi**2/r**2 * (A_derivative - 2*A/r) + z_3*(A_derivative/r**2 - 2*A/r**3) * p_r**4)
+
+  return [dr_dt, dvarphi_dt, dp_varphi_dt, dp_r_dt]
+
+# nu needs to be defined as a constant, as it isn't changing
+def check_event_horizon(t, y):
+  r = y[0]
+  return calc_A(nu, 1/r)
+check_event_horizon.terminal = True
+check_event_horizon.direction = -1
+
+# initial_conditions = [r_0, varphi_0, p_varphi_0, p_r_0]
+initial_conditions = [15, 0, calc_p_varphi(nu, 1/15), 0]
+solution = scipy.integrate.solve_ivp(derivatives, t_span = (0, 100000), y0=initial_conditions, events=check_event_horizon, dense_output=True)
+
+# results
+print(solution.t, solution.y[0], solution.y[1])
+for i in range(len(solution)):
+  print(i, solution.t[i], solution.y[0][i], solution.y[1][i])
