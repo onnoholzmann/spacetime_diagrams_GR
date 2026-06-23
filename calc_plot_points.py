6dcaa8ddffb027da1767418ccd72f22ebfa616d2
Onno Holzmann <143614975+onnoholzmann@users.noreply.github.com>
2026-06-23 20:20:11 +0200

    ADD the inspiral motion

--- calc_plot_points.py
+++ calc_plot_points.py
@@ -1,6 +1,8 @@
 import scipy
 # imported the things that are used more often seperately, but might be better to use either full aliasing, or none
 import numpy
+import math
+import mpmath
 from functools import cache
 
 """
@@ -66,6 +68,52 @@ def calc_T_X(t, r):
 def calc_z_3(nu):
   return 2*nu * (4 - 3*nu)
 
+nu_pole=1/numpy.sqrt(3)
+"""dubble check if it needs nu or velocity(v)"""
+def remove_calc_f_taylor(nu):
+  f_taylor = 1 - 1247*nu**2/336 + 4*numpy.pi*nu**3 - 44711*nu**4/9072 - 8191*numpy.pi*nu**5/672 - 16285*numpy.pi*nu**7/504
+  f_taylor += nu**6*(6643739519/6985440 - 1712*numpy.euler_gamma/105 + 16*numpy.pi**2/3 - 3424*math.log(2)/105 - 1712*math.log(nu)/105)
+  f_taylor += nu**8*(-323105549467/323105549467 + 232597*numpy.euler_gamma/4410 - 1369*numpy.pi**2/126 + 39931*math.log(2)/294 - 47385*math.log(3)/1568 + 232597*math.log(nu)/4410)
+  return f_taylor
+def calc_f_taylor_cooficients(nu):
+  return [1, 0, -1247*nu**2/336, 4*numpy.pi*nu**3, -44711*nu**4/9072, -8191*numpy.pi*nu**5/672,
+                        nu**6*(6643739519/6985440 - 1712*numpy.euler_gamma/105 + 16*numpy.pi**2/3 - 3424*math.log(2)/105 - 1712*math.log(nu)/105),
+                        -16285*numpy.pi*nu**7/504,
+                        nu**8*(-323105549467/323105549467 + 232597*numpy.euler_gamma/4410 - 1369*numpy.pi**2/126 + 39931*math.log(2)/294 - 47385*math.log(3)/1568 + 232597*math.log(nu)/4410)]
+# f_taylor_cooficients = [1, 0, -1247*nu**2/336, 4*numpy.pi*nu**3, -44711*nu**4/9072, -8191*numpy.pi*nu**5/672,
+                        # nu**6*(6643739519/6985440 - 1712*numpy.euler_gamma/105 + 16*numpy.pi**2/3 - 3424*math.log(2)/105 - 1712*math.log(nu)/105),
+                        # -16285*numpy.pi*nu**7/504,
+                        # nu**8*(-323105549467/323105549467 + 232597*numpy.euler_gamma/4410 - 1369*numpy.pi**2/126 + 39931*math.log(2)/294 - 47385*math.log(3)/1568 + 232597*math.log(nu)/4410)]
+
+# def calc_shifted_f_taylor(nu_varphi):
+  # return (1 - nu_varphi/nu_pole) * calc_f_taylor(nu_varphi)
+
+# p, q = mpmath.pade(calc_shifted_f_taylor, 4, 4)
+# p = [float(i) for i in p]
+# q = [float(i) for i in q]
+def calc_f_resummed(nu_varphi):
+  f_taylor_cooficients = calc_f_taylor_cooficients(nu_varphi)
+  shifted_f_taylor_cooficients = [None for _ in range(9)]
+  for i in range(9):
+    shifted_f_taylor_cooficients[i] = f_taylor_cooficients[i]
+    if i > 0:
+      shifted_f_taylor_cooficients[i] -= f_taylor_cooficients[i]/nu_pole
+    # if i < len(f_taylor_cooficients) - 1:
+    #   shifted_f_taylor_cooficients[i+1] -= f_taylor_cooficients[i]/nu_pole
+  p, q = mpmath.pade(shifted_f_taylor_cooficients, 4, 4)
+  p = [float(i) for i in p]
+  q = [float(i) for i in q]
+
+  # part_formula = (1 - nu_varphi/nu_pole)
+  # nu_dummy = numpy.linspace(0, 0.001, 9) # create 9 close points
+  # shifted_dummy = part_formula * numpy.array([calc_f_taylor(nu) for nu in nu_dummy])
+  # get the cooficients
+  # cooficients = numpy.polyfit(nu_dummy, shifted_dummy, 8)
+  numinator = numpy.polyval(p[::-1], nu_varphi)
+  denuminator = numpy.polyval(q[::-1], nu_varphi)
+
+  return (numinator / denuminator) / (1 - nu_varphi/nu_pole)
+
 # H is the hamiltonian
 # def calc_H_eff(p_r, p_varphi, nu, r):
   # return numpy.sqrt(p_r**2 + calc_A(nu, 1/r) * (1 + p_varphi**2/r**2 + calc_z_3(nu) * p_r**4/r**2))
@@ -90,14 +138,29 @@ def derivatives(t, y):
   u=1/r
   A = calc_A(nu, u)
   A_derivative = calc_A_derivative(nu, u)
+  # A_derivative == dA/du
+  # use the chain rule to convert to dA/dr
+  dA_dr = A_derivative * (-u**2)
   D = calc_D(nu, u)
   H = calc_H(p_r, p_varphi, nu, r)
   H_eff = calc_H_eff(p_r, p_varphi, nu, r)
+  # 
+  # 
+  # f_varphi = -32/5*varphi**5 # needs continueing
 
   # the equations of motion
+  # Omega == dvarphi_dt
   dvarphi_dt = (A*p_varphi) / (nu*r**2*H*H_eff)
   dr_dt = A/numpy.sqrt(D) * 1/(nu*H*H_eff) * (p_r + z_3 * (2*A)/(r**2) * p_r**3)
-  dp_varphi_dt = 0
+
+  # nu_varphi = r * dvarphi_dt
+  # Omega**5*r**4 == (nu_varphi**15 / r)
+  psi = 2/r**2 * (1/dA_dr) * (1 + 2*nu*(numpy.sqrt(A * (p_varphi**2/r**2)) - 1))
+  r_omega = r*psi**(1/3)
+  v_varphi = dvarphi_dt * r_omega
+  f_resummed = calc_f_resummed(nu)
+  f_varphi = -32/5 * nu * dvarphi_dt**5*r**4 * f_resummed
+  dp_varphi_dt = f_varphi
   # it is assumed 0, since we assume no radiation(friction in this case), at least for now
   dp_r_dt = -A/numpy.sqrt(D) * 1/(2*nu*H*H_eff) * (A_derivative + p_varphi**2/r**2 * (A_derivative - 2*A/r) + z_3*(A_derivative/r**2 - 2*A/r**3) * p_r**4)
 
